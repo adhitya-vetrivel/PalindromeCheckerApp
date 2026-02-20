91c1952024395f3e7821334af769ae36c945cade
unknown <av6802@srmist.edu.in>
2026-02-20 09:15:04 +0530

    adhitya Added usecase2

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker App!");
+        System.out.println("modify this logic for usecase 2");
+    }
+}    
+
