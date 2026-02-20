777b4c8e97c2cbb77b2d7884a366dceb4e740aba
unknown <av6802@srmist.edu.in>
2026-02-20 09:54:19 +0530

    adhitya added usecase1

--- UseCase1PalindromeCheckerApp.java
+++ UseCase1PalindromeCheckerApp.java
@@ -32,8 +32,8 @@ public class UseCase1PalindromeCheckerApp {
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Welcome to the Palindrome  Checker Management System");
         System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        System.out.println("System initialized  successfully.");
     }
 }
